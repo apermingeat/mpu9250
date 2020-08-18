ea8baa5d8c562dd6ee281e169023cceca782ebeb
Alejandro Permingeat <apermingeat@gmail.com>
2020-08-18 11:56:26 -0300

    Se obtiene la inicializació desde una implementación
    Se obtiene de:
    https://github.com/vedranMv/tm4c_mpu9250/blob/master/mpu9250/
    
    Cabe la aclaración de que el magnetómetro está implementado en un die diferente (AK8963). Para acceder al magnetómetro, se hace via el MPU9250.
    
    Queda pendiente la calibración del giróscopo.

--- mpu9250.c
+++ mpu9250.c
@@ -6,6 +6,9 @@
 
 #include <linux/i2c.h>
 #include <linux/regmap.h>
+
+#include <linux/delay.h>
+
 #include "mpu9250.h"
 
 /*Se define un major number arbitrario (estoy inventando) */
@@ -18,6 +21,11 @@ static struct cdev my_dev;
  * conectado al bus*/
 
 static struct i2c_client *slaveDevice;
+
+static u8 Gscale = GFS_250DPS;
+static u8 Ascale = AFS_2G;
+static u8 Mscale = MFS_16BITS;
+static u8 Mmode = M_100HZ;
 
 /*********************************************************************************
  * Definiciones de funciones sobre archivos y estructura correspondiente
@@ -122,8 +130,144 @@ static int mpu9250_isAlive(struct i2c_client *client)
 	if (WHO_AM_I_RESPONSE == mpu9250_read(client, WHO_AM_I))
 		ret = 0;
 
-	return ret;
+	return ret;	
+}
+
+
+
+/* Inicialización del modulo MPU9250 obtenido de:
+   https://github.com/vedranMv/tm4c_mpu9250/blob/master/mpu9250/api_mpu9250.c
+*/
+
+/**
+ * Configure MPU9250 accelerometer and gyroscope
+ * Configures gyro for 1kHz sampling rate, 42Hz bandwidth and 200Hz output rate
+ * and use full scale readings (+/- 250 dps). Configures accel for 1kHz sampling
+ * rate, 200Hz output rate and full scale readings (+/- 16g). Finally, configure
+ * interrupt pin to be active high, push-pull, held high until cleared and
+ * cleared by reading ANY register. I2C bypass is disabled to allow for SPI.
+ * Data-ready interrupts are only allowed.
+ */
+static int mpu9250_accel_gyro_init(struct i2c_client *client)
+{
+	float gBias[3], aBias[3];
+	u8 c;
+
+	int ret = mpu9250_isAlive(client);
+
+	if (ret < 0)
+		return ret;
+
+	//calibrateMPU9250(gBias, aBias);
+
+	// wake up device
+	// Clear sleep mode bit (6), enable all sensors
+	mpu9250_write(client, PWR_MGMT_1, 0x00);
+	msleep(100); // Wait for all registers to reset
+
+	// Get stable time source
+	// Auto select clock source to be PLL gyroscope reference if ready else
+	mpu9250_write(client, PWR_MGMT_1, 0x01);
+	msleep(200);
+
+	// Configure Gyro and Thermometer
+	// Disable FSYNC and set thermometer and gyro bandwidth to 41 and 42 Hz,
+	// respectively;
+	// minimum delay time for this setting is 5.9 ms, which means sensor fusion
+	// update rates cannot be higher than 1 / 0.0059 = 170 Hz
+	// DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for both
+	// With the MPU9250, it is possible to get gyro sample rates of 32 kHz (!),
+	// 8 kHz, or 1 kHz
+	mpu9250_write(client, CONFIG, 0x03);
+
+	// Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
+	// Use a 200 Hz rate; a rate consistent with the filter update rate
+	// determined inset in CONFIG above.
+	mpu9250_write(client, SMPLRT_DIV, 0x00);
+
+	// Set gyroscope full scale range
+	// Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are
+	// left-shifted into positions 4:3
+
+	// get current GYRO_CONFIG register value
+	c = mpu9250_read(client, GYRO_CONFIG);
+	// c = c & ~0xE0; // Clear self-test bits [7:5]
+	c = c & ~0x02; // Clear Fchoice bits [1:0]
+	c = c & ~0x18; // Clear AFS bits [4:3]
+	c = c | Gscale << 3; // Set full scale range for the gyro
+	// Set Fchoice for the gyro to 11 by writing its inverse to bits 1:0 of
+	// GYRO_CONFIG
+	c |= 0x03;
+	// Write new GYRO_CONFIG value to register
+	mpu9250_write(client, GYRO_CONFIG, c);
+
+	// Set accelerometer full-scale range configuration
+	// Get current ACCEL_CONFIG register value
+	c = mpu9250_read(client, ACCEL_CONFIG);
+	// c = c & ~0xE0; // Clear self-test bits [7:5]
+	c = c & ~0x18;  // Clear AFS bits [4:3]
+	c = c | Ascale << 3; // Set full scale range for the accelerometer
+	// Write new ACCEL_CONFIG register value
+	mpu9250_write(client, ACCEL_CONFIG, c);
+
+	// Set accelerometer sample rate configuration
+	// It is possible to get a 4 kHz sample rate from the accelerometer by
+	// choosing 1 for accel_fchoice_b bit [3]; in this case the bandwidth is
+	// 1.13 kHz
+	// Get current ACCEL_CONFIG2 register value
+	c = mpu9250_read(client, ACCEL_CONFIG2);
+	c = c & ~0x0F; // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])
+	c = c | 0x03;  // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
+	// Write new ACCEL_CONFIG2 register value
+	mpu9250_write(client, ACCEL_CONFIG2, c);
+	// The accelerometer, gyro, and thermometer are set to 1 kHz sample rates,
+	// but all these rates are further reduced by a factor of 5 to 200 Hz because
+	// of the SMPLRT_DIV setting
+
+	// Configure Interrupts and Bypass Enable
+	// Set interrupt pin active high, push-pull, hold interrupt pin level HIGH
+	// until interrupt cleared, clear on read of any register, and DISABLE
+	// I2C_BYPASS_EN -> otherwise communication with AK8963 doesn't work when
+	//  using SPI
+	mpu9250_write(client, INT_PIN_CFG, 0x30);
+	// Enable data ready (bit 0) interrupt
+	mpu9250_write(client, INT_ENABLE, 0x01);
+	msleep(100);
 	
+	ret = 0;
+
+	return ret;
+}
+
+/**
+ * Initialize AK8963 magnetometer. 16bit data @ 100Hz
+ * AK8963 is configured as a slave device of MPU9250 and not directly accessible.
+ * Every transaction with AK8963 has to be carried through MPU9250 by configuring
+ * its I2C slave communication registers. Result of the I2C transaction is also
+ * stored internally in MPU and needs to be read as one would normally read
+ * MPU registers.
+ */
+static int mpu9250_mag_init(struct i2c_client *client)
+{
+    //  Initialization uses I2C channel number 4 for writing data
+
+    //  Configure master I2C clock (400kHz) for MPU to talk to slaves
+    mpu9250_write(client,  I2C_MST_CTRL, 0x5D);
+    //  Enable I2C master
+    mpu9250_write(client,  USER_CTRL, 0x20);
+
+    //  Stop I2C slave number 4
+    mpu9250_write(client,  I2C_SLV4_CTRL, 0x00);
+    //  Set address for I2C4 slave to that of AK8963, writing mode (MSB=0)
+    mpu9250_write(client,  I2C_SLV4_ADDR, AK8963_ADDRESS);
+    //  Select which register is being updated
+    mpu9250_write(client,  I2C_SLV4_REG, AK8963_CNTL);
+    // Set value to write into the register:
+    //      16-bit continuous measurements @ 100Hz
+    mpu9250_write(client,  I2C_SLV4_DO, Mscale << 4 | Mmode);
+    // Trigger write data to slave device 4 -> AK8963
+    mpu9250_write(client,  I2C_SLV4_CTRL, 0x80);
+    msleep(5);
 }
 
 /**********************************************************************
@@ -196,7 +340,22 @@ static int mpu9250_i2c_probe(struct i2c_client *client,
 		return ret;
 	}
 
-	pr_info("\tMPU9250 is alive\n");
+	ret = mpu9250_accel_gyro_init(client);
+
+	if (ret < 0){
+		unregister_chrdev_region(dev, 1);
+		pr_info("Error during MPU9250 Accel and Gyro initialization.\n");
+		return ret;
+	}
+
+	ret = mpu9250_mag_init(client);
+
+	if (ret < 0){
+		unregister_chrdev_region(dev, 1);
+		pr_info("Error during MPU9250 Magnetometer initialization.\n");
+		return ret;
+	}
+	pr_info("\tMPU9250 is alive and initialized.\n");
 	
 	return(0);
 }
